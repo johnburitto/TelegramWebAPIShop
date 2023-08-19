08f17cdd69247dc84414b9e990e34bd2022b215e
johnburitto <johnburitto@gmail.com>
2023-08-20 01:34:38 +0300

    Add docker compose file with database, migrate tables to db and create empty WebAPI project

--- Core/Entities/Discount.cs
+++ Core/Entities/Discount.cs
@@ -6,10 +6,30 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public float DiscoutInPercent { get; set; }
-        public float NormalizedDiscount => DiscoutInPercent / 100;
+        public float NormalizedDiscount
+        {
+            get
+            {
+                return DiscoutInPercent / 100;
+            }
+            set
+            {
+                
+            }
+        }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public DiscountStatus Status => DateTime.UtcNow < StartDate || DateTime.Now > EndDate ? DiscountStatus.Inactive : DiscountStatus.Active;
+        public DiscountStatus Status
+        {
+            get
+            {
+                return DateTime.UtcNow < StartDate || DateTime.Now > EndDate ? DiscountStatus.Inactive : DiscountStatus.Active;
+            }
+            set
+            {
+
+            }
+        }
 
         public List<Product>? Products { get; set; }
     }
